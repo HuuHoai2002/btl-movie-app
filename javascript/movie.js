6f92027df519bf591fb4bdb2ba969f2741b90a77
Nghiem Huu Hoai <Huuhoai0511@gmail.com>
2022-07-17 22:24:24 +0700

    .

--- javascript/movie.js
+++ javascript/movie.js
@@ -1,6 +1,7 @@
 import { api_key, base_url, data_type } from "../config/config.js";
 import { renderListMovie } from "../utils/render.js";
 import { $ } from "../utils/selector.js";
+import { setTitle } from "../utils/setTitle.js";
 
 const movie_list = $(".movie-list");
 const params = new URLSearchParams(window.location.search);
@@ -11,5 +12,6 @@ const popular_url = `${base_url}/movie/${data_type.POPULAR}?api_key=${api_key}&l
 
 async function Main() {
   await renderListMovie(popular_url, movie_list);
+  setTitle(`Phim chiếu rạp | trang ${page}`);
 }
 Main();
